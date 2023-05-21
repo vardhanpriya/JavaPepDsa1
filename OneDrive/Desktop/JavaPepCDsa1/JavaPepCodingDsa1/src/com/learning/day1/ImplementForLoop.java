854d9f88eefab39d35bc1760c619f1ccace525d9
priya vardhan <vardhanpriya001@gmail.com>
2023-05-21 21:48:37 +0530

    implementloop

---
+++ OneDrive/Desktop/JavaPepCDsa1/JavaPepCodingDsa1/src/com/learning/day1/ImplementForLoop.java
@@ -0,0 +1,23 @@
+package com.learning.day1;
+
+public class ImplementForLoop {
+
+    public static void main(String[] args){
+
+        for(int i=1; i<=20; i++){
+
+            if(i%2==0){
+                System.out.print(i + " ");
+            }
+       }
+
+            System.out.println();
+        for (int i=1; i<=20; i++){
+
+            if( i%2 != 0){
+                System.out.print(i + " ");
+
+            }
+        }
+    }
+}
